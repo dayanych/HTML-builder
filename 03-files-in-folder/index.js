3f4fc9262baf6f41240fe35e03ab35975ff890c6
Dayana <dayana.shakenova20@gmail.com>
2023-05-02 21:08:17 +0600

    feat: 3th task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,1 +1,19 @@
-process.on('exit', () => process.stdout.write('Удачи в изучении Node.js!'));
+const fs = require('fs');
+const path = require('path');
+
+const pathJoin = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathJoin, { withFileTypes: true }, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      if (!file.isDirectory()) {
+        fs.stat(`${pathJoin}/${file.name}`, (error, stats) => {
+          console.log(file.name + ' - ' + path.extname(file.name).slice(1, path.extname(file.name).length) + ' - ' + stats.size / 1024 + 'kb');
+        });
+      }
+    });
+  }
+});
+
