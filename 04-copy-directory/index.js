721e60b16508d0558b618bc3e452d71821e8c679
Dayana <dayana.shakenova20@gmail.com>
2023-05-03 21:33:18 +0600

    feat: 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+const filesDir = path.join(__dirname, 'files');
+const filesCopyDir = path.join(__dirname, 'files-copy');
+
+async function backupFiles() {
+  try {
+    await fs.mkdir(filesCopyDir, { recursive: true });
+    const files = await fs.readdir(filesDir);
+    for (const file of files) {
+      const sourcePath = path.join(filesDir, file);
+      const destPath = path.join(filesCopyDir, file);
+      await fs.copyFile(sourcePath, destPath);
+    }
+    const filesCopy = await fs.readdir(filesCopyDir);
+    for (const fileCopy of filesCopy) {
+      if (!files.includes(fileCopy)) {
+        const deletePath = path.join(filesCopyDir, fileCopy);
+        await fs.unlink(deletePath);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+backupFiles();
